f7b99550018697ec95e6cd88bd66eb86df4fcfce
Smelly Crocodile <perrin1057@gmail.com>
2024-09-13 10:35:36 -0700

    updating auth to use actual user id

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,18 +1,19 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError, UTFiles } from "uploadthing/server";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 30 } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 50 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req, files }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const session = await getServerSession(options);
+      const user = await session?.user;
 
       // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
@@ -22,12 +23,10 @@ export const ourFileRouter = {
       });
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id, [UTFiles]: fileOverrides };
+      return { userId: user.email, [UTFiles]: fileOverrides };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-
-      console.log("file url", file.url);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
