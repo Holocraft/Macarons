9f1e99d02904db38a49d2046761db23ad85c4f05
Smelly Crocodile <perrin1057@gmail.com>
2024-09-22 08:27:37 -0700

    added date/time to comment

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,7 +1,9 @@
 export default function Comment({ comment }) {
   return (
     <div className='comment-container'>
-      <p className='comment-user'>{comment.author.name}</p>
+      <p className='comment-user'>{`${
+        comment.author.name
+      } ${comment.createdAt.toLocaleString("en-US")}`}</p>
       <p>{comment.content}</p>
     </div>
   );
