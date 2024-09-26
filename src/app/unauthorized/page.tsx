0008356a1cbc631c38cfc47180a3e2495225f1c0
Smelly Crocodile <perrin1057@gmail.com>
2024-09-26 12:14:20 -0700

    added styles for unauthorized text

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,8 +1,12 @@
 export default function Unauthorized() {
   return (
     <div>
-      <h1>Sorry, you are not authorized to log in.</h1>
-      <h2>Contact the site admin to request access.</h2>
+      <h1 style={{ color: "#3f3844" }}>
+        Sorry, you are not authorized to log in.
+      </h1>
+      <h2 style={{ color: "#3f3844" }}>
+        Contact the site admin to request access.
+      </h2>
     </div>
   );
 }
